6054e3f332658782793f7a73bb6f1cecea71d591
20220255 <118017744+20220255@users.noreply.github.com>
2025-01-01 16:15:03 +0800

    1. Updated seeds data

--- prisma/seeds/dealership.ts
+++ prisma/seeds/dealership.ts
@@ -1,44 +1,67 @@
 export const Dealership = [
-    {
-      "id": "001",
-      "name": "BG Philippines",
-      "address1": "35 F. Manalo St. Kabayanan",
-      "address2": "San Juan Metro Manila 1500",
-      "mobile": " 0917-827-5407",
-      "landline": null,
-      "contactPerson": "BG ContactPerson",
-      "customerVehicleId": null,
-      "customerId": null,
-      "createdAt": "2022-09-27T18:00:00.000Z",
-      "updatedAt": "2022-09-27T18:00:00.000Z",
-      "enteredBy": null
-    },
-    {
-      "id": "002",
-      "name": "Trident Auto Luxury Philippines",
-      "address1": "Ground Floor Primex Tower, EDSA, corner Connecticut",
-      "address2": "San Juan, 1503 Metro Manila",
-      "mobile": " 0906 476 6666",
-      "landline": "(02) 1111 1111",
-      "contactPerson": "Trident ContactPerson",
-      "customerVehicleId": null,
-      "customerId": null,
-      "createdAt": "2022-09-27T18:00:00.000Z",
-      "updatedAt": "2022-09-27T18:00:00.000Z",
-      "enteredBy": null
-    },
-    {
-      "id": "003",
-      "name": "PGA Cars, Inc.",
-      "address1": "201 Epifanio de los Santos Ave,",
-      "address2": "Mandaluyong, 1556 Metro Manila",
-      "mobile": "0906 476 6667",
-      "landline": "(02) 8727 0381",
-      "contactPerson": "PGA ContactPerson",
-      "customerVehicleId": null,
-      "customerId": null,
-      "createdAt": "2022-09-27T18:00:00.000Z",
-      "updatedAt": "2022-09-27T18:00:00.000Z",
-      "enteredBy": null
-    }
-  ]
+  {
+    "name": "Trident Auto Luxury Philippines",
+    "address1": "Ground Floor Primex Tower, EDSA, corner Connecticut",
+    "address2": "San Juan, 1503 Metro Manila",
+    "mobile": " 0906 476 6666",
+    "landline": "(02) 1111 1111",
+    "contactPerson": "Trident ContactPerson",
+    "customerVehicleId": null,
+    "enteredBy": null,
+    "createdAt": "2022-09-27 18:00:00",
+    "updatedAt": "2022-09-27 18:00:00",
+    "id": 1
+  },
+  {
+    "name": "PGA Cars, Inc.",
+    "address1": "201 Epifanio de los Santos Ave,",
+    "address2": "Mandaluyong, 1556 Metro Manila",
+    "mobile": "0906 476 6667",
+    "landline": "(02) 8727 0381",
+    "contactPerson": "PGA ContactPerson",
+    "customerVehicleId": null,
+    "enteredBy": null,
+    "createdAt": "2022-09-27 18:00:00",
+    "updatedAt": "2022-09-27 18:00:00",
+    "id": 2
+  },
+  {
+    "name": "BG Philippines",
+    "address1": "35 F. Manalo St. Kabayanan",
+    "address2": "San Juan Metro Manila 1500",
+    "mobile": " 0917-827-5407",
+    "landline": null,
+    "contactPerson": "BG ContactPerson",
+    "customerVehicleId": null,
+    "enteredBy": null,
+    "createdAt": "2022-09-27 18:00:00",
+    "updatedAt": "2022-09-27 18:00:00",
+    "id": 3
+  },
+  {
+    "name": "Toyota Pasig",
+    "address1": "124 Eulogio Rodriguez Jr. Ave",
+    "address2": "Pasig, 1800 Metro Manila",
+    "mobile": "09111111111",
+    "landline": "(02) 7238 6777",
+    "contactPerson": "Toyota Contactperson",
+    "customerVehicleId": null,
+    "enteredBy": null,
+    "createdAt": "2024-12-29 18:19:25.929",
+    "updatedAt": "2024-12-29 18:19:25.929",
+    "id": 4
+  },
+  {
+    "name": "Park N' Sell",
+    "address1": "Meralco Ave, corner Capt. Henry P. Javier, Ortigas Center",
+    "address2": "Pasig, Metro Manila",
+    "mobile": "0900-7009579",
+    "landline": "(02) 7000 9579",
+    "contactPerson": "Park N' Sell Contact Person",
+    "customerVehicleId": null,
+    "enteredBy": null,
+    "createdAt": "2025-01-01 07:45:59.454",
+    "updatedAt": "2025-01-01 07:45:59.454",
+    "id": 5
+  }
+]
