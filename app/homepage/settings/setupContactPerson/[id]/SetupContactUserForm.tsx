f13e032b0a55572218c559b5a97ca73769ed4668
20220255 <118017744+20220255@users.noreply.github.com>
2025-01-01 16:05:40 +0800

    1.  Set dealershipId to Number type

--- app/homepage/settings/setupContactPerson/[id]/SetupContactUserForm.tsx
+++ app/homepage/settings/setupContactPerson/[id]/SetupContactUserForm.tsx
@@ -80,7 +80,7 @@ const SetupContactUserForm = ({ user, error, roles, dealerships }: { user?: User
                                 id="dealershipId"
                                 required
                                 value={dealershipId ?? ''}
-                                onChange={(e) => setDealershipId(e.target.value)}
+                                onChange={(e) => setDealershipId(Number(e.target.value))}
                                 name='dealershipId'
                                 className="fom-control"
                                 
